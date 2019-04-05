c45ec1a9c4afe7efe63582e906907458d1520187
Ititi Obidah <iobidah9@gmail.com>
2019-04-05 15:50:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,30 +1,26 @@
-def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |name, i|
+def roll_call_dwarves(dwarfs)
+  dwarfs.each_with_index do |name, i|
     puts "#{i + 1}. #{name}"
-    # Your code here
+  end
+end 
+def summon_captain_planet(veggies)
+  veggies.collect do |word|
+    word.capitalize + "!"
   end
 end
-
-def summon_captain_planet(veggies)
-  veggies.collect do |call|
-    call.capitalize + "!"
-  end # Your code here
-end
-
-def long_planeteer_calls(calls_long)
-  calls_long.any? do |word|
+def long_planeteer_calls(calls)
+  calls.any? do |word|
    if word.length > 4
-      true
+     true 
    else
      false
-   end
+  end
  end
 end
-
 def find_the_cheese(cheese)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese.find do |cheese|
-    cheese_types.include?(cheese)
+  cheese_types.include?(cheese)
  end
-end
+end 
+
